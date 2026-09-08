5ea87a472016718064644a218f09c835a743fad1
Pramila K <pramilak.2412@gmail.com>
2026-09-08 17:47:05 +0530

    appointments list created

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,6 +7,7 @@ import Login from '../pages/Login/Login';
 import ForgotPassword from '../pages/ForgotPassword/ForgotPassword';
 
 import Dashboard from '../pages/Dashboard/Dashboard';
+import Appointments from '../pages/Appointments/Appointments';
 import Patients from '../pages/Patients/Patients';
 import AddPatient from '../pages/Patients/AddPatient';
 import PatientDetails from '../pages/Patients/PatientDetails';
@@ -39,6 +40,7 @@ const AppRoutes = () => {
 
       <Route element={<DashboardLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/appointments" element={<Appointments />} />
         
         <Route path="/patients" element={<Patients />} />
         <Route path="/patients/add" element={<AddPatient />} />
